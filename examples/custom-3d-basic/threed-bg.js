8c7f083997a716a78b55d3a24653bed27c7eda66
cjgammon <gammon@adobe.com>
2019-11-11 23:01:53 -0600

    add basic 3d deck

--- examples/custom-3d-basic/threed-bg.js
+++ examples/custom-3d-basic/threed-bg.js
@@ -1,33 +1,103 @@
 
-
 class Background{
     constructor(selector) {
-        this.scale = 4;
-
+        this.perspective = 35;
         this.canvas = document.querySelector(selector);
+        
+        this.createThreeScene();
 
-        this.renderer = new THREE.WebGLRenderer({canvas: this.canvas});
+        setTimeout(() => this.createDomScene(), 100);
+    }
+
+    createDomScene() {
+        let hud = document.querySelector('.athena-hud'); //move outside camera
+        document.body.appendChild(hud);
+
+        this.domCamera = document.querySelector('#athena-root');
+        this.domCamera.style.position = 'absolute';
+        this.domCamera.style.transform = 'translate3d(0, 0, 0)';
+        this.domCamera.style.perspectiveOrigin = 'center center';
+        this.domCamera.style.left = '50%';
+        this.domCamera.style.top = '50%';
+        this.domCamera.style.transformStyle = 'preserve-3d';
+
+        document.body.style.perspective = `${this.perspective}px`;//'754.866px';
+        document.body.style.width = '100vw';
+        document.body.style.height = '100vh';
+        document.body.style.overflow = 'hidden';
+    }
+
+    createThreeScene() {
+        this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
         this.renderer.setPixelRatio( window.devicePixelRatio );
         this.renderer.setSize( window.innerWidth, window.innerHeight );
 
-        this.camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.01, 10000 );
-        this.camera.position.set( 0, 50, 3000 );
+        this.cameraRig = new THREE.Object3D();
+
+        this.camera = new THREE.PerspectiveCamera( this.perspective, window.innerWidth / window.innerHeight, 0.01, 10000 );
+        this.cameraRig.add( this.camera );
 
         this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color( 0xff0000 );
+        this.scene.background = new THREE.Color( 0x333333 );
+        this.scene.fog = new THREE.Fog( 0x333333, 1000, 2000 );
 
-        var light = new THREE.DirectionalLight( 0xffffff );
-        light.position.set( 0, 0, 1 );
-        this.scene.add( light );
+        this.scene.add( this.cameraRig );
+
+        var ambient = new THREE.AmbientLight( 0x333333 );
+        this.scene.add( ambient );
+
+        var pointlight = new THREE.PointLight( 0xffffff, 0.5 );
+        pointlight.position.set( 0, -9, 0 );
+        this.cameraRig.add( pointlight );
+
+        let geometry = new THREE.PlaneGeometry( 1000, 10000, 100, 1000 );
+        for (let i = 0; i < geometry.vertices.length; i++) {
+            geometry.vertices[i].z -= Math.random() * 10;
+        }
+        geometry.computeFlatVertexNormals();
+
+        let material = new THREE.MeshLambertMaterial({ color: 0xefefef, flatShading: true });
+
+        this.mesh = new THREE.Mesh(geometry, material)
+        this.mesh.position.set(0, -10, -5000);
+        this.mesh.rotation.x = -90 * Math.PI / 180;
+        this.scene.add(this.mesh);
+
+        let eventBus = Athena.eventBus;
+        let SlideEvent = Athena.events.SlideEvent;
+        eventBus.subscribe(SlideEvent.ANIMIN, () => this.animIn());
 
         this.render();
     }
 
     render() {
-        
+        if (this.domCamera){
+            let _z = -parseFloat(this.cameraRig.position.z);
+            this.domCamera.style.transform = `translate3d(0px, 0px, ${_z}px)`;
+        }
         this.renderer.render(this.scene, this.camera);
+    }
 
-        requestAnimationFrame(() => this.render());
+    animIn() {
+        let transitionDuration = 1;
+        let slideInfo = Athena.slides;
+        let slides = slideInfo.slides;
+        let current = slideInfo.currentSlide;
+
+        let currentSlide = slides[current];
+        let el = currentSlide.el;
+
+        if (!el.dataset.pos) {
+            return;
+        }
+
+        let pos = el.dataset.pos;
+
+        let tl = new TimelineLite({
+            onUpdate: () => this.render()
+        });
+
+        tl.to(this.cameraRig.position, transitionDuration, {z: -pos});
     }
 }
 
--- examples/custom-3d-basic/threed-slide.js
+++ examples/custom-3d-basic/threed-slide.js
@@ -1,11 +1,25 @@
-class ThreeD extends Athena.slideTypes.step{
+class ThreeD extends Athena.slideTypes.basic{
+
     constructor(_index, _el) {
         super(_index, _el);
         this.duration = _el.dataset.transitionDuration || 0.2;
+        this.pos = _el.dataset.pos;
+
         _el.style.opacity = '0';
         _el.style.transition = `opacity ${this.duration}s linear`;
-    }
+        _el.style.transform = `translate3d(0, 0, -${this.pos}px)`;
+        _el.style.transformOrigin = `center center`;
+        _el.style.top = '-50vh';
+        _el.style.left = '-50vw';
+        _el.style.width = '100vw';
+        _el.style.height = '100vh';
+        _el.style.background = 'rgba(255, 0, 0, 0.2)';
 
+        setTimeout(() => {
+
+        }, 100);
+    }
+    
     animIn() {
         super.animIn();
         this.el.style.opacity = '1';
@@ -15,18 +29,19 @@ class ThreeD extends Athena.slideTypes.step{
         let SlideEvent = Athena.events.SlideEvent;
 
         return new Promise((resolve, reject) => {
-            console.log('animout ', this.id);
             this.el.style.opacity = '0';
             this.bus.dispatch(SlideEvent.ANIMOUT);
 
             clearTimeout(this.to);
             this.to = setTimeout(() => {
-                console.log('animout complete', this.id);
                 this.setCurrent(false);
                 this.bus.dispatch(SlideEvent.DISSOLVE);
                 resolve();
             }, this.duration * 1000);
         });
+        
     }
+    
 };
+
 Athena.slideTypes.threed = ThreeD;
